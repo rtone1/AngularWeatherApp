c3fa9915bc0dfb29ea708805309d273279159094
Rodrigo Torres <rtdesigns@yahoo.com>
2016-01-09 17:39:39 -0600

    set up dependencies and file structure

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      build: {
+        src: 'public/javascripts/app.js',
+        dest: 'public/javascripts/build/app.min.js'
+      }
+    },
+
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'public/stylesheets/build/main.css': 'public/stylesheets/styles.scss',
+        }
+      }
+    },
+    // Watch for changes!
+    watch: {
+      options: {
+        livereload: true,
+        spawn: false // Very important, don't miss this
+      },
+      scripts: {
+        files: ['public/javascripts/build/app.min.js', 'public/javascripts/app.js'],
+        tasks: ['uglify']
+      },
+      // images: {
+      //   files: 'images/*.{png,jpg,gif}',
+      //   tasks: ['imagemin']
+      // },
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      }
+    },
+    // Inject changes into the browser!
+    browserSync: {
+          bsFiles: {
+            src : [
+            '**/*.scss',
+            '**/*.js',
+            '**/*.html'
+            ]
+          },
+          options: {
+            proxy: 'localhost:9000',
+            watchTask: true
+          }
+        }
+
+  }); // end of task
+
+  // Load the plugin that provides the "uglify" and "sass" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+
+  // Default task(s).
+  grunt.registerTask('default', ['browserSync', 'watch']);
+
+};
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # AngularWeatherApp
 Weather App using AngularJS and Weather Channel API
+
+1. I need a weather App
+2. It should display 7 day forecast
+3. It should use user location as a starting point
+4. It should give the user the ability to enter unique locations
+5. It should allow the user to view forecast in celsius or fahrenheit
+
+Bonus
+1. Give me some cool animations
+2. have it automatically refresh every 2 hours  
