116431a88764f8fdffc093cc3b14ead739a290fc
Rodrigo Torres <rtdesigns@yahoo.com>
2016-01-11 19:33:54 -0600

    added weatherApp module and WeatherController that makes call to third party API

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,20 @@
+(function(){
+  console.log('Lets do this!');
 
+  var app = angular.module( 'weatherApp', [] );
+
+  app.controller( 'WeatherController', ['$http', function($http){
+    var weather = this;
+    weather.forecasts = [];
+    $http.get('http://api.wunderground.com/api//forecast10day/q/IL/Chicago.json').success(function(data){
+      var options = data;
+      weather.forecasts = options.forecast.simpleforecast.forecastday;
+    });
+  }]);
+
+
+
+
+
+
+})(); //END OF CLOUSER
